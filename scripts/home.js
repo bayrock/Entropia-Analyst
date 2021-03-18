e95ce375d93b94f3e886e4329e9f0cb7ce4ca486
Bailey Chestnut <bayrock@users.noreply.github.com>
2021-03-18 02:28:35 -0400

    Calculate skill totals and log to console

--- scripts/home.js
+++ scripts/home.js
@@ -40,21 +40,44 @@ function handleFiles(files) {
 
 function parseFile(file) {
     const reader = new FileReader()
-    reader.onload = event => matchSkills(event.target.result)
+    reader.onload = event => readSkills(event.target.result)
     reader.onerror = error => console.error(error)
 
     reader.readAsText(file)
 }
 
-function matchSkills(chatlog) {
+function readSkills(chatlog) {
     const pattern = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) \[System\] \[\] You have gained (\d\.*\d*) (.+)/g
 
-    let matches, skills = []
-    while (matches = pattern.exec(chatlog))
-        skills.push(matches[3])
+    // Match the skill gains
+    let matches, startDate, skillLabels = [], skillGains = []
+    while (matches = pattern.exec(chatlog)) {
+        if (!startDate)
+            startDate = matches[1]
 
-    skills = skills.map(skill => skill.remove("experience in your ").remove(" skill"))
-    console.log(skills)
+        skillGains.push(matches[2])
+        skillLabels.push(matches[3])
+    }
+
+    skillLabels = skillLabels.map(skill => skill.remove("experience in your ").remove(" skill"))
+    
+    // Calculate the totals
+    let gain, total, skillData = {}
+    skillLabels.forEach((label, key) => {
+        gain = parseFloat(skillGains[key])
+        total = skillData[label]
+        skillData[label] = total? total + gain : 0 + gain
+        //console.log(label, skillGains[key])
+    })
+
+    // Log skill data to console
+    const length = Object.keys(skillData).length
+    for (const skill in skillData) {
+        total = skillData[skill].toFixed(4)
+        console.log(`%c${skill}:%c ${total}`, "font-weight:bold;", "font-weight:normal;")
+    }
+
+    console.log(`%cShowing ${length} gains since ${startDate}`, "font-weight:bold;color:green;")
 }
 
 String.prototype.remove = function(str) { return this.replace(str, "") }
